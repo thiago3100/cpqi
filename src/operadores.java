f25ef1fe36100bc1cff11f27dd3e346ebef98de6
thiago3100 <thfelipe31@gmail.com>
2022-06-07 20:31:41 -0300

    adicionando os operadores

---
+++ src/operadores.java
@@ -0,0 +1,15 @@
+public class operadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       
+        int x = 9 + 4;
+        double y = 8 - 4 + 3 * 2;
+        
+        System.out.println(x);
+        System.out.print(y);
+       
+    }
+}
