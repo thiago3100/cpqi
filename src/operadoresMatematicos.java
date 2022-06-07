53949faf57ae165b2efbc9475ea7b10d590e4b89
thiago3100 <thfelipe31@gmail.com>
2022-06-07 20:45:58 -0300

    adicionando os operadores matemáticos

---
+++ src/operadoresMatematicos.java
@@ -0,0 +1,13 @@
+public class operadoresMatematicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int x = 6;
+        int y = ++x;
+        
+        System.out.println("x = " + x);
+        System.out.println("y = " +y);
+    }
+}
