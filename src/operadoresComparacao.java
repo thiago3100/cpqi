903ce20f8b4c9f9d4a6a600b4d461e08df8b370a
thiago3100 <thfelipe31@gmail.com>
2022-06-08 09:15:48 -0300

    adicionando operadores de comparação

---
+++ src/operadoresComparacao.java
@@ -0,0 +1,15 @@
+public class operadoresComparacao {
+
+  
+    public static void main(String args[]) {
+       
+        int x = 6;
+        
+        System.out.println(x > 7);
+        System.out.println(x == 6);
+        System.out.println(x >= 7);
+        System.out.println(x != 5);
+        System.out.println("oi" instanceof String);
+        
+    }
+}
