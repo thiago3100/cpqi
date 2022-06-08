4e1156ce170588996f2b860d5688ce0dfa826d07
thiago3100 <thfelipe31@gmail.com>
2022-06-08 09:22:57 -0300

    adicionando operadores lógicos

---
+++ src/operadoresLogicos.java
@@ -0,0 +1,16 @@
+public class operadoresLogicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       
+        int x = 6;
+        
+        System.out.println((x > 7) && (x <=10));
+        System.out.println((x > 3) || (x <=8));
+        System.out.println(!(x > 2) && (x !=6));
+        
+        
+    }
+}
